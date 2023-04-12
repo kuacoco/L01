bdadc7274c52671dac9552023bc04019605bc7fb
kuacoco <kuacoco@qq.com>
2023-04-12 15:38:10 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::controller('StaticPagesController')->group(function () {
     Route::get('/about', 'about')->name('about');
 });
 Route::get('signup', 'UsersController@create')->name('signup');
+
+//Route::get('/users', 'UsersController@index')->name('users.index');
+//Route::get('/users/create', 'UsersController@create')->name('users.create');
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::post('/users', 'UsersController@store')->name('users.store');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+//Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+Route::resource('users', 'UsersController');
