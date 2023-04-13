f4b247fc23569a29eb8a1e1e0263072bb1a151f7
kuacoco <kuacoco@qq.com>
2023-04-13 15:22:16 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的用户名和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+    }
+}
--- lang/zh_CN/validation.php
+++ lang/zh_CN/validation.php
@@ -3,7 +3,7 @@
 return [
 
     'attributes' => [
-        'name' => '名称',
+        'name' => '用户名',
         'email' => '邮箱',
         'password' => '密码',
     ],
